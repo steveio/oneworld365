989da9ec6b7e45f8fabd2e8708bcf0f7f9ddeddd
steveio.com <info@steveio.com>
2024-04-09 11:52:26 +0100

    404 page

---
+++ 404.php
@@ -0,0 +1,123 @@
+
+	
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+<title>Travel, Work, Intern, Teach, Volunteer or Study Abroad</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<meta name="google-site-verification" content="lNJMDbpe5zfy_6ap1x7GMmPiu7cCTQ_fNQW0ToD7SSU" />
+<meta name="description" content="Search thousands of trips, tours, volunteer programs, jobs, internships & courses around the world." />
+<meta name="keywords" content="world,volunteer,programs,tours,courses,around,jobs,search,trips,internships,thousands" />
+
+<meta property="og:title" content="Travel, Work, Intern, Teach, Volunteer or Study Abroad" />
+<meta property="og:url" content="http://www.oneworld365.org/" />
+<meta property="og:description" content="Search thousands of trips, tours, volunteer programs, jobs, internships & courses around the world." />
+
+<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="/css/style.css" media="screen" />
+
+
+</head>
+
+<body>
+
+<header class="border-bottom">
+<div class="container">
+ 
+<div class="row my-3">
+    <div class="col-8">
+	<div class="col-lg-4 col-sm-12">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+          <img src="http://www.oneworld365.org/images/oneworld365_logo_small.png" alt="Travel, Work, Intern, Teach, Volunteer or Study Abroad" border="0" />
+        </a>
+        </div>
+    </div>        
+</div>
+
+
+
+<div class="alert alert-danger" role="alert">
+  <h2>404 Not Found</h2>
+</div>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" fill="currentColor" class="bi bi-signpost-split" viewBox="0 0 16 16">
+  <path d="M7 7V1.414a1 1 0 0 1 2 0V2h5a1 1 0 0 1 .8.4l.975 1.3a.5.5 0 0 1 0 .6L14.8 5.6a1 1 0 0 1-.8.4H9v10H7v-5H2a1 1 0 0 1-.8-.4L.225 9.3a.5.5 0 0 1 0-.6L1.2 7.4A1 1 0 0 1 2 7zm1 3V8H2l-.75 1L2 10zm0-5h6l.75-1L14 3H8z"/>
+</svg>
+
+<!-- BEGIN Footer -->
+
+<footer class="py-3 my-4">
+
+<div class="container">
+<div class="align-items-center justify-content-center">
+
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+			<li class="nav-item"><a class="nav-link px-2 text-muted" href="/logout">Logout</a></li>
+	        <li class="nav-item"><a class="nav-link px-2 text-muted" href="/about-us" title="About">About</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-muted" href="/disclaimer" title="Disclaimer">Disclaimer</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-muted" href="/privacy" title="Privacy">Privacy Policy</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-muted" href="/company/a-z" title="Company A-Z">Company A-Z</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-muted" href="/advertising" title="Advertise">Advertise</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-muted" href="/press" title="Press">Press</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-muted" href="/contact" title="Contact">Contact</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">Work, Volunteer, Teach, Travel, Intern & Learn Worldwide</p>
+
+    <div class="row">
+        <div class="">
+            <div class="footer-icon">
+                <h4>Follow Our Social Media Pages</h4>
+        		<div class="col-12 my-3">
+                        <a href="http://www.facebook.com/oneworld365" target="_blank"><img alt="One World 365 Facebook" src="http://www.oneworld365.org/img/101/facebook_84_50.png" /></a>
+                        <a href="http://www.instagram.com/oneworld365" target="_blank"><img alt="One World 365 Instagram" src="http://www.oneworld365.org/img/101/instagram_84_50.png" /></a>
+                        <a href="http://www.twitter.com/oneworld365" target="_blank"><img alt="One World 365 Twitter" src="http://www.oneworld365.org/img/101/twitter_84_50.jpg" /></a>
+                        <a href="http://www.pinterest.com/oneworld365" target="_blank"><img alt="One World 365 Pinterest" src="http://www.oneworld365.org/img/101/pinterest_84_50.png" /></a>
+        		</div>
+            
+                <h4>Partners &amp; Featured In</h4>
+        		<div class="col-12 my-3">
+                    	<img alt="FCO" src="http://www.oneworld365.org/img/101/fco_84_50.png" />
+                    	<img alt="BBC Worldwide" src="http://www.oneworld365.org/img/101/bbc_worldwide_84_50.png" /> 
+                    	<img alt="USA Today" src="http://www.oneworld365.org/img/101/usa_today_84_50.png" />
+                    	<img alt="Rough Guides" src="http://www.oneworld365.org/img/101/rough_guides_84_50.jpg" />
+                    	<img alt="Lonely Planet" src="http://www.oneworld365.org/img/101/lonely_planet_84_50.png" />
+                    	<img alt="National Geographic" src="http://www.oneworld365.org/img/101/Natgeo_84_50.png" />
+            	</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-3">
+    <p class="small text-center text-body-secondary">One World 365 &copy;  2007 - 2024</p>
+    </div>
+    
+</div>
+</div>
+
+</footer>
+
+<!-- END Footer -->
+
+
+
+</form>
+</div>
+
+ 
+
+</html>
+
